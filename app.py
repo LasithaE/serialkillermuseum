00e158e2da60994e43974951bfcb1c555368a951
LasithaE <62329100+LasithaE@users.noreply.github.com>
2020-12-28 23:39:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template,request, url_for,redirect
-import os
 from bs4 import BeautifulSoup as bs
 import requests
 app = Flask(__name__)
@@ -69,5 +68,4 @@ def test():
 def about():
     return render_template('about.html')
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(threaded=True, port=5000)
